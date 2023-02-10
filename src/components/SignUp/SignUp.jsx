e5db19c85beed3858df356b97e39263548117b69
Ivan <offlane@tut.by>
2023-02-10 15:52:15 +0300

    form

---
+++ src/components/SignUp/SignUp.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+export const SignUp = ({ title, handleClick }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  return (
+    <div>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="enter email"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="enter password"
+      />
+      <button onClick={handleClick}>{title}</button>
+    </div>
+  );
+};
