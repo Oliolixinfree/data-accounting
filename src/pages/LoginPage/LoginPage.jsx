d380c25ea9f2d7a22de2c1cfbfd821e4649925e7
Ivan <offlane@tut.by>
2023-02-10 15:26:28 +0300

    firebase connected

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import styles from './LoginPage.module.scss';
 import { Link } from 'react-router-dom';
 
 export const LoginPage = () => {
   return (
     <>
       <h1>LoginPage</h1>
-      <Link to="/register">Register</Link>
+      <Link to="/register">
+        <span className={styles.link}>Register</span>
+      </Link>
     </>
   );
 };
--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import styles from './Register.module.scss';
 import { Link } from 'react-router-dom';
 
 export const RegisterPage = () => {
   return (
     <>
       <h1>RegisterPage</h1>
-      Already have an account? <Link to="/login">Sign in</Link>
+      Already have an account?
+      <Link to="/login">
+        <span className={styles.link}> Sign in</span>
+      </Link>
     </>
   );
 };
